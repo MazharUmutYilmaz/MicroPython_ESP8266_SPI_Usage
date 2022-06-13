ef7a9d14e7a5b286d26dc8fd60229ed10f41c4a3
MazharUmutYilmaz <69629353+MazharUmutYilmaz@users.noreply.github.com>
2022-06-13 13:25:58 +0300

    Add files via upload

---
+++ SPI_Usage.py
@@ -0,0 +1,8 @@
+import max7219
+from machine import Pin, SPI
+spi = SPI(1, baudrate=10000000, polarity=0, phase=0)
+screen = max7219.Matrix8x8(spi, Pin(15), 1)
+screen.brightness(5)
+screen.fill(0)
+screen.text('3',0,0,1)
+screen.show()
